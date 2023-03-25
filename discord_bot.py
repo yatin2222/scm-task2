39d5da94e1b04db9adf0673de214582f4201b447
lucifersyth <lucifersyth@gmail.com>
2023-03-26 00:42:30 +0530

    Bot Added

---
+++ discord_bot.py
@@ -0,0 +1,40 @@
+if message.content.startswith("$hdfc2020"):
+        await message.channel.send(file=discord.File('hdfc 2020.png'))
+        await message.channel.send('For current information on the stocks please click on the link https://www.moneycontrol.com/india/stockpricequote/banks-private-sector/hdfcbank/HDF01')
+        await message.channel.send('For more stocks related updates please click on the link https://finance.yahoo.com/')
+      if message.content.startswith("$hdfc2021"):
+        await message.channel.send(file=discord.File('hdfc 2021.png'))
+        await message.channel.send('For current information on the stocks please click on the link https://www.moneycontrol.com/india/stockpricequote/banks-private-sector/hdfcbank/HDF01')
+        await message.channel.send('For more stocks related updates please click on the link https://finance.yahoo.com/')
+    if message.content.startswith("$kotak"):
+      await message.channel.send('Please enter the year for which you would like to observe the graph.For Year-2020 Please type $kotak2020,For Year-2021 Please type $kotak2021')
+      if message.content.startswith("$kotak2020"):
+        await message.channel.send(file=discord.File('kotak 2020.png'))
+        await message.channel.send('For current information on the stocks please click on the link https://www.moneycontrol.com/india/stockpricequote/banks-private-sector/kotakmahindrabank/KMB')
+        await message.channel.send('For more stocks related updates please click on the link https://finance.yahoo.com/')
+      if message.content.startswith("$kotak2021"):
+        await message.channel.send(file=discord.File('kotak 2021.png'))
+        await message.channel.send('For current information on the stocks please click on the link https://www.moneycontrol.com/india/stockpricequote/banks-private-sector/kotakmahindrabank/KMB')
+        await message.channel.send('For more stocks related updates please click on the link https://finance.yahoo.com/')
+    if message.content.startswith("$itc"):
+      await message.channel.send('Please enter the year for which you would like to observe the graph.For Year-2020 Please type $itc2020,For Year-2021 Please type $itc2021')
+      if message.content.startswith("$itc2020"):
+        await message.channel.send(file=discord.File('itc 2020.png'))
+        await message.channel.send('For current information on the stocks please click on the link https://www.moneycontrol.com/india/stockpricequote/diversified/itc/ITC')
+        await message.channel.send('For more stocks related updates please click on the link https://finance.yahoo.com/')
+      if message.content.startswith("$itc2021"):
+        await message.channel.send(file=discord.File('itc 2021.png'))
+        await message.channel.send('For current information on the stocks please click on the link https://www.moneycontrol.com/india/stockpricequote/diversified/itc/ITC')
+        await message.channel.send('For more stocks related updates please click on the link https://finance.yahoo.com/')
+    if message.content.startswith("$airtel"):
+      await message.channel.send('Please enter the year for which you would like to observe the graph.For Year-2020 Please type $airtel2020,For Year-2021 Please type $airtel2021') 
+      if message.content.startswith("$airtel2020"):
+        await message.channel.send(file=discord.File('airtel 2020.png'))
+        await message.channel.send('For current information on the stocks please click on the link https://www.moneycontrol.com/india/stockpricequote/telecommunications-service/bhartiairtel/BA08')
+        await message.channel.send('For more stocks related updates please click on the link https://finance.yahoo.com/')
+      if message.content.startswith("$airtel2021"):
+        await message.channel.send(file=discord.File('airtel 2021.png'))
+        await message.channel.send('For current information on the stocks please click on the link https://www.moneycontrol.com/india/stockpricequote/telecommunications-service/bhartiairtel/BA08')
+        await message.channel.send('For more stocks related updates please click on the link https://finance.yahoo.com/')
+keep_alive()
+client.run('MTA0NjY3NjAzNzIxMzM1NjEyMg.GMUgGo.aIPHEsWz8dewlE9wQ2Nxjs_zMvwJSdrVvfURDA')
